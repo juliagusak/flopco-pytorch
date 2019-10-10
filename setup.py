dbdb1208e546be01a51f51c46c8bf10b5c8fed93
juliagusak <julgusak@gmail.com>
2019-10-10 19:50:35 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -16,12 +16,12 @@ def load_requirements(file_name):
 
 setup(
     name="flopco-pytorch",
-    version="0.1.0",
+    version="v0.1.0",
     description="FLOPs and other statistics COunter for Pytorch neural networks",
     author="Julia Gusak",
     author_email="julgusak@gmail.com",
     url="https://github.com/juliagusak/flopco-pytorch",
-    download_url="https://github.com/juliagusak/flopco-pytorch/archive/v_0.1.0.tar.gz",
+    download_url="https://github.com/juliagusak/flopco-pytorch/archive/v0.1.0.tar.gz",
     keywords = ['pytorch', 'flops', 'macs', 'neural-networks', 'cnn'],
     license="MIT",
     packages=find_packages(),
